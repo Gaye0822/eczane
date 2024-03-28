097d5782e29e16ae137afd03b0f1fdeb29155063
helinmelike <helinmelikec@gmail.com>
2024-03-28 17:51:14 +0300

    Update WinFormsApp1.AssemblyInfo.cs

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820b1cd29499473721bff794497f741b707420dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7ec0f54811a656bc76295342ec565a2339102c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
