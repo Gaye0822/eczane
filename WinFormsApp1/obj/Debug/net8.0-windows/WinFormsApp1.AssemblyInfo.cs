9c5885f5f0530a33fda7d771f9900b0ffed3b8e1
Gaye Çetindere <cetinderegaye@gmail.com>
2024-03-29 12:48:46 +0300

    g

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f00e80c442c78cc38ec7dd5f5ed8fdad7d1537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb2a1366a4ef7851e94d87e63deab2e0d63c1c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
